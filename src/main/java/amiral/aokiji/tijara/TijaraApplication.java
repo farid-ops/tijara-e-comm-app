7393f4a801df07f46f8ac7a3d19e2b0ba066ec29
snowden <faridibnchouaib89@gmail.com>
2021-12-09 02:23:49 +0100

    init

---
+++ src/main/java/amiral/aokiji/tijara/TijaraApplication.java
@@ -0,0 +1,13 @@
+package amiral.aokiji.tijara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TijaraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TijaraApplication.class, args);
+    }
+
+}
---
+++ src/test/java/amiral/aokiji/tijara/TijaraApplicationTests.java
@@ -0,0 +1,13 @@
+package amiral.aokiji.tijara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TijaraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
