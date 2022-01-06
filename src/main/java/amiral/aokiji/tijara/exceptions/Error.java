959a5d63c93822ec7b762685f85cb8ca8c04d4c6
snowden <faridibnchouaib89@gmail.com>
2022-01-06 23:27:16 +0100

    errors

--- src/main/java/amiral/aokiji/tijara/exceptions/Error.java
+++ src/main/java/amiral/aokiji/tijara/exceptions/Error.java
@@ -1,2 +1,39 @@
-package amiral.aokiji.tijara.exceptions;public class Error {
+package amiral.aokiji.tijara.exceptions;
+
+import lombok.NoArgsConstructor;
+import lombok.Getter;
+import org.apache.logging.log4j.util.Strings;
+
+@NoArgsConstructor
+@Getter
+public class Error {
+    private String errorCode;
+    private String message;
+    private Integer statusCode;
+    private String url = "Not available";
+    private String reqMethod = "Not available";
+
+    public void setErrorCode(String errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public void setStatusCode(Integer statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    public Error setUrl(String url) {
+        if (Strings.isNotBlank(url))
+            this.url = url;
+        return this;
+    }
+
+    public Error setReqMethod(String reqMethod) {
+        if (Strings.isNotBlank(reqMethod))
+            this.reqMethod = reqMethod;
+        return this;
+    }
 }
--- src/main/java/amiral/aokiji/tijara/exceptions/ErrorUtils.java
+++ src/main/java/amiral/aokiji/tijara/exceptions/ErrorUtils.java
@@ -1,2 +1,16 @@
-package amiral.aokiji.tijara.exceptions;public class ErrorUtils {
+package amiral.aokiji.tijara.exceptions;
+
+public class ErrorUtils {
+
+    private ErrorUtils(){
+        super();
+    }
+
+    public static Error createError(String errorCode, String message, Integer status){
+        Error error = new Error();
+        error.setErrorCode(errorCode);
+        error.setMessage(message);
+        error.setStatusCode(status);
+        return error;
+    }
 }
