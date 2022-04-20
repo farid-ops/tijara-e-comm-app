f59822db6a7ea473e2eb03a8c8a4b6a073c17918
snowden <faridibnchouaib89@gmail.com>
2022-04-20 13:10:44 +0100

    declaratiion des methodes

--- src/main/java/amiral/aokiji/tijara/core/service/ItemServiceImpl.java
+++ src/main/java/amiral/aokiji/tijara/core/service/ItemServiceImpl.java
@@ -1,46 +1,48 @@
 package amiral.aokiji.tijara.core.service;
 
 import amiral.aokiji.tijara.core.model.ItemEntity;
-import amiral.aokiji.tijara.core.model.ProductEntity;
 import amiral.aokiji.tijara.swagger.ItemDTO;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
-import java.util.UUID;
 import java.util.stream.Collectors;
+
 
 @Service
 public class ItemServiceImpl implements ItemService {
 
+
+    /*
+    * Transforme une entity Item en un POJO ItemDTO.
+    * */
     @Override
-    public ItemEntity toEntity(ItemDTO itemDTO) {
-        ItemEntity itemEntity = new ItemEntity();
-        itemEntity.setProductEntity(new ProductEntity().setId(UUID.fromString(itemDTO.getId())))
-                .setPrice(itemDTO.getPrice()).setQuantity(itemDTO.getQuantity());
-        return itemEntity;
+    public ItemEntity mapItemDTOToItem(ItemDTO itemDTO) {
+        ModelMapper mapper = new ModelMapper();
+        ItemEntity item = mapper.map(itemDTO, ItemEntity.class);
+        return item;
     }
 
     @Override
-    public List<ItemEntity> toEntityList(List<ItemDTO> itemDTOS) {
+    public List<ItemEntity> mapItemDTOSToItems(List<ItemDTO> itemDTOS) {
         if (Objects.isNull(itemDTOS))
             return Collections.emptyList();
-        return itemDTOS.stream().map(this::toEntity).collect(Collectors.toList());
+        return itemDTOS.stream().map(this::mapItemDTOToItem).collect(Collectors.toList());
     }
 
     @Override
-    public ItemDTO toModel(ItemEntity itemEntity) {
-        ItemDTO itemDTO = new ItemDTO();
-        itemDTO.setId(itemEntity.getProductEntity().getId().toString())
-                .setPrice(itemEntity.getPrice()).setQuantity(itemEntity.getQuantity());
+    public ItemDTO mapItemEntityToItemDTO(ItemEntity itemEntity) {
+        ModelMapper mapper = new ModelMapper();
+        ItemDTO itemDTO = mapper.map(itemEntity, ItemDTO.class);
         return itemDTO;
     }
 
     @Override
-    public List<ItemDTO> toModelList(List<ItemEntity> itemEntities) {
+    public List<ItemDTO> mapItemEntityToItemDTOS(List<ItemEntity> itemEntities) {
         if (Objects.isNull(itemEntities))
             return Collections.emptyList();
-        return itemEntities.stream().map(this::toModel).collect(Collectors.toList());
+        return itemEntities.stream().map(this::mapItemEntityToItemDTO).collect(Collectors.toList());
     }
 }
